33e587cd0371ff2505f9ad82403e5332c3467361
wilmost <wilmomartinezs@gmail.com>
2023-08-06 14:59:12 -0400

    updated api request path

--- resume-site/js/setupjs.js
+++ resume-site/js/setupjs.js
@@ -1,5 +1,5 @@
 
-fetch('https://2sjnb63exh.execute-api.us-east-1.amazonaws.com/Prod/hello')
+fetch('https://2sjnb63exh.execute-api.us-east-1.amazonaws.com/Prod/update-web-visit')
     .then(response => response.json())
     .then((data) => {
         document.getElementById('counter').innerText = data.count
