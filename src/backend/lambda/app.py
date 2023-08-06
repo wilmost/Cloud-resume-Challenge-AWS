38213e48d10ee29a5ea7ec98a9d772eea7b1f3e7
wilmost <wilmomartinezs@gmail.com>
2023-08-06 14:53:06 -0400

    added lambda funtion

---
+++ src/backend/lambda/app.py
@@ -0,0 +1,34 @@
+import json
+import boto3  
+
+
+dynamodb = boto3.resource('dynamodb')
+table_name = 'cloud-resume-challenge'
+dynamodb_table = dynamodb.Table(table_name)
+
+
+def lambda_handler(event, context):
+    response = dynamodb_table.get_item(Key={"ID": "visitors"})
+    currentVisitCount = int(response["Item"]["visitors"]) + 1
+    dynamodb_table.update_item(
+        Key={"ID": "visitors"},
+        UpdateExpression='ADD Visitors :inc',
+        ExpressionAttributeValues={':inc': 1}
+                               
+    )
+    
+    
+    data = {"visits": currentVisitCount} 
+    
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(data),
+        "headers": {
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+    }
+    
+    return response
+    
