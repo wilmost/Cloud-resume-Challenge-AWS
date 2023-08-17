5996c0f1a4f70f418b447642f665b1ec1564a14e
wilmost <wilmomartinezs@gmail.com>
2023-08-16 20:17:42 -0400

    moved to src dir

--- resume-site/js/setupjs.js
+++
@@ -1,6 +0,0 @@
-
-fetch('https://2sjnb63exh.execute-api.us-east-1.amazonaws.com/Prod/update-web-visit')
-    .then(response => response.json())
-    .then((data) => {
-        document.getElementById('counter').innerText = data.visitors
-    })
